192354e7edcb815076de759248f80ab40ff90c47
josh16 <josh.randall16@gmail.com>
2018-06-08 13:44:30 -0400

    Import Project
    -Importing project

---
+++ Core RPG/Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // == Constants ==
+    private const string PLAYER = "Player";
+
+    GameObject Player;
+    
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag(PLAYER);
+        Debug.Log("Found player!");
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position;
+    }
+
+
+}
