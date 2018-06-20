55ce2a9ac37c3849c028742fc51a81c88b9a50ec
josh16 <josh.randall16@gmail.com>
2018-06-19 23:27:21 -0400

    CursorAffordance script
    -Created cursor affordance script which will deal with changing the cursor sprite depending on what layer the cursor is on.

---
+++ Core RPG/Assets/Scripts/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour 
+{
+    [SerializeField]
+    Texture2D walkCursor = null;
+
+    [SerializeField]
+    Texture2D targetCursor = null;
+
+    [SerializeField]
+    Texture2D unknownCursor = null;
+
+
+    [SerializeField]
+    CursorMode cursorMode = CursorMode.Auto;
+
+    [SerializeField]
+    Vector2 hotSpot = new Vector2(96, 96);
+
+    CameraRaycaster cameraRaycaster;
+
+    // Use this for initialization
+	void Start () 
+    {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+	}
+	
+	void Update () 
+    {
+        switch (cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, hotSpot, cursorMode);
+                break;
+
+            case Layer.Enemy:
+                Cursor.SetCursor(targetCursor, hotSpot, cursorMode);
+                break;
+
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unknownCursor, hotSpot, cursorMode);
+                break;
+
+            default:
+                Debug.LogError("Don't know what cursor to show ");
+                return;
+        }
+
+        
+	}
+}
